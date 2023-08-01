5d8e71e6aba0656bfd074b16c8e64a8c50055466
Juan Bressan <juan.bressan@nevly.com>
2023-08-01 17:09:52 -0300

    loading state added

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,19 @@
-const Loading = () => <div>Loading</div>
+'use client'
+import Box from '@mui/material/Box'
+import CircularProgress from '@mui/material/CircularProgress'
 
-export default Loading
+export default function Loading() {
+	return (
+		<Box
+			sx={{
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				width: '100%',
+				height: '100vh',
+			}}
+		>
+			<CircularProgress />
+		</Box>
+	)
+}
