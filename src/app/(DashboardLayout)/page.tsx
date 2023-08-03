a46a97aba47112533ae5c222419f14b4aa94dab1
Juan Bressan <juanrb83@gmail.com>
2023-08-03 16:00:02 -0300

    unused pages/components removed

--- src/app/(DashboardLayout)/page.tsx
+++ src/app/(DashboardLayout)/page.tsx
@@ -2,42 +2,12 @@
 
 import { Box, Grid } from '@mui/material'
 
-import Blog from '@/app/(DashboardLayout)/components/dashboard/Blog'
-import MonthlyEarnings from '@/app/(DashboardLayout)/components/dashboard/MonthlyEarnings'
 import PageContainer from '@/app/(DashboardLayout)/components/container/PageContainer'
-import ProductPerformance from '@/app/(DashboardLayout)/components/dashboard/ProductPerformance'
-import RecentTransactions from '@/app/(DashboardLayout)/components/dashboard/RecentTransactions'
-// Components
-import SalesOverview from '@/app/(DashboardLayout)/components/dashboard/SalesOverview'
-import YearlyBreakup from '@/app/(DashboardLayout)/components/dashboard/YearlyBreakup'
 
 const Dashboard = () => (
-	<PageContainer title="Dashboard" description="this is Dashboard">
+	<PageContainer title="Dashboard" description="dashboard homepage">
 		<Box>
-			<Grid container spacing={3}>
-				<Grid item xs={12} lg={8}>
-					<SalesOverview />
-				</Grid>
-				<Grid item xs={12} lg={4}>
-					<Grid container spacing={3}>
-						<Grid item xs={12}>
-							<YearlyBreakup />
-						</Grid>
-						<Grid item xs={12}>
-							<MonthlyEarnings />
-						</Grid>
-					</Grid>
-				</Grid>
-				<Grid item xs={12} lg={4}>
-					<RecentTransactions />
-				</Grid>
-				<Grid item xs={12} lg={8}>
-					<ProductPerformance />
-				</Grid>
-				<Grid item xs={12}>
-					<Blog />
-				</Grid>
-			</Grid>
+			<Grid container spacing={3}></Grid>
 		</Box>
 	</PageContainer>
 )
--- src/app/(DashboardLayout)/users/page.tsx
+++ src/app/(DashboardLayout)/users/page.tsx
@@ -5,10 +5,10 @@ import React, { Suspense } from 'react'
 import PageContainer from '../components/container/PageContainer'
 import DashboardCard from '../components/shared/DashboardCard'
 
-export default function Settings() {
+export default function Users() {
 	return (
-		<PageContainer title="Settings" description="Settings page">
-			<DashboardCard title="Settings">
+		<PageContainer title="Users" description="Users page">
+			<DashboardCard title="Users">
 				<Suspense
 					fallback={
 						<p style={{ textAlign: 'center' }}>loading... on initial request</p>
